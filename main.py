ef21e95ba8b3dce0e46348ddf4c99f226e988f1b
gaurangsharma76 <99582050+gaurangsharma76@users.noreply.github.com>
2022-03-07 18:28:43 +0530

    Add files via upload
    I have made changes in password manager and it can also search for existing details in file by searching websites and the details are saved in JSON file

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from tkinter import messagebox
 import random
+import json
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
@@ -37,17 +38,55 @@ def add_entry():
     email_entered = email_input.get()
     password_entered = password_input.get()
     website_entered = website_input.get()
+    new_data = {
+        website_entered: {
+            "email": email_entered,
+            "password": password_entered
+        }
+
+    }
+
     if (email_entered == "") or (password_entered == "") or (website_entered == ""):
         messagebox.showwarning(title="Missing fields", message="Please enter all fields")
     else:
-        with open("data.txt", "a") as password_file:
-            password_file.write(
-                f"website:{website_entered}| EmailID/Username:{email_entered}| password:{password_entered}\n")
+        try:
+            with open("data.json", "r") as password_file:
+                # reading old data
+                data = json.load(password_file)
+                # updating new data
+                data.update(new_data)
+            # writing updated data to file
+            with open("data.json", "w") as password_file:
+                json.dump(data, password_file, indent=4)
+
+        except FileNotFoundError:
+            with open("data.json", "w") as password_file:
+                json.dump(new_data, password_file, indent=4)
+
         password_input.delete(0, END)
         website_input.delete(0, END)
         email_input.delete(0, END)
         messagebox.showinfo(title="Success", message="Details saved successfully")
         website_input.focus()
+
+
+def find_password():
+    website = website_input.get()
+    if len(website) == 0:
+        messagebox.showerror(title="No website entered", message="Please enter website to search")
+    else:
+        try:
+            with open("data.json", "r") as data_file:
+                data = json.load(data_file)
+        except FileNotFoundError:
+            messagebox.showwarning(title="Not found", message="Details matching with entered website is not found")
+        else:
+            for websites in data:
+                if websites == website:
+                    messagebox.showinfo(title="Details",
+                                        message=f"Email:{data[websites]['email']} \n Password:{data[websites]['password']}")
+                else:
+                    messagebox.showerror(title="Details not found", message="Details of website entered not found")
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -64,8 +103,8 @@ canvas.grid(row=0, column=1)
 website_label = Label(text="Website")
 website_label.grid(row=1, column=0)
 
-website_input = Entry(width=35)
-website_input.grid(row=1, column=1, columnspan=2)
+website_input = Entry(width=17)
+website_input.grid(row=1, column=1)
 website_input.focus()
 
 email_label = Label(text="Email/Username")
@@ -85,5 +124,8 @@ generate_password_button.grid(row=3, column=2)
 
 add_button = Button(text="Add", highlightthickness=0, width=35, command=add_entry)
 add_button.grid(row=4, column=1, columnspan=2)
+
+Search_button = Button(text="Search", highlightthickness=0, command=find_password)
+Search_button.grid(row=1, column=2)
 
 window.mainloop()
